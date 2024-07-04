f5944aad833d81a684cec5b9532adcfcc631a5c0
sajolkk <sajolkar56@gmail.com>
2024-07-04 23:22:38 +0600

    project initialize

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h6 className='text-center text-3xl font-medium p-3'>Currency Converter</h6>
+    </>
+  )
+}
+
+export default App
